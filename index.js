7084afe2d07d1236a1bdd94f475fb6881f1afce4
Ian Cossentino <icossentino@gmail.com>
2021-03-02 11:43:44 -0500

    commit tree 860d4a81fb3191bd2209f1a42f0a26e5c22436b9

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+
+
+
+function returnFirstTwoDrivers(drivers) {
+  const twoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+  }
+  return twoDrivers(drivers)
+}
+
+
+
+function returnLastTwoDrivers(drivers) {
+  const twoDrivers = function(drivers) {
+    return drivers.slice(-2)
+  }
+  return twoDrivers(drivers)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(mult) {
+  return function(fare) {
+    return fare * mult
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, driverFunc) {
+  return driverFunc(drivers)
+}
